4ad82404e6f5b17865d8c4ef7680e8178fe8f575
ِAlhanouf Almansour <50381191+haan0@users.noreply.github.com>
2021-08-26 16:33:50 +0300

    Add files via upload

---
+++ process.java
@@ -0,0 +1,18 @@
+
+package srtf;
+
+
+public class process {
+    public int pid;
+    public int arival;
+    public int burst;
+    
+    
+    public process(int pid,int arival,int burst){
+        this.pid=pid;
+        this.arival=arival;
+        this.burst=burst;  
+    }
+    
+   
+}
